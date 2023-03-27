3d62cd19399fb3269190ae5f2ceb693a4e5e25e4
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-03-27 13:46:11 +0330

    fix

--- blog/views.py
+++ blog/views.py
@@ -1,43 +1,25 @@
+from .serializers import *
 from django.shortcuts import render
-from rest_framework import serializers
-from rest_framework.views import APIView, Response, status
-from .models import  Posts, Comments, PostLikes
-from server.authenticate import Authenticate
-from django.contrib.auth.models import User
 from django.db import IntegrityError
-
-class UserSerializer(serializers.ModelSerializer):
-   class Meta:
-      model = User
-      fields = '__all__'
-
-class PostSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Posts
-    fields = '__all__'
-
-class CommentSerializer(serializers.ModelSerializer):
-  class Meta:
-    model = Comments
-    fields = '__all__'
+from django.contrib.auth.models import User
+from server.authenticate import Authenticate
+from .models import  Posts, Comments, PostLikes
+from rest_framework.views import APIView, Response, status
 
 class Authe(APIView):
-  def __init__(self, ):
-    self.SECRET_KEY = 'python_jwt'
-
-  def post(self, request):
-    data = request.data
+  def post(self, req):
+    username = req.data['phone']
+    del req.data['phone']
     try:
-      user = User.objects.create_user(username=data['username'],email=data['email'], password=data['password'])
-
+      user = User.objects.create_user(username=username, **req.data)
       user = UserSerializer(user).data
 
       return Response({'status':status.HTTP_201_CREATED, 'data': user})
     except IntegrityError:
-      return Response({"status": status.HTTP_409_CONFLICT, "error":"email already exists"})
+      return Response({"status": status.HTTP_409_CONFLICT, "error":"phone already exists"})
     except Exception as err:
       print('error:', err)
-      return Response(status=status.HTTP_400_BAD_REQUEST)
+      return Response({'status': status.HTTP_400_BAD_REQUEST, 'error': str(err)})
 
 
 class Post(APIView):
@@ -50,22 +32,20 @@ class Post(APIView):
 
   def post(self, req):
     print('user: ', req)
-    data = req.data
-    post = Posts.objects.create(author_id=1 ,title=data['title'], content=data['content'], subject=data['subject'], image=data['image'])
+    post = Posts.objects.create(author_id=1 , **req.data)
     post = PostSerializer(post).data
 
     return Response({'status': status.HTTP_201_CREATED, 'posts': post})
 
 
   def put(self, req, post_id=None):
-    data = req.data
     if post_id is not None:
       try:
+        Posts.objects.filter(id=post_id).update(**req.data)
+
         post = Posts.objects.get(id=post_id)
-        post.title = data['title']
-        post.content = data['content']
-        post.save()
         post = PostSerializer(post).data
+
         return Response({'status':status.HTTP_200_OK, 'post':post})
       except Posts.DoesNotExist:
         return Response({'status': status.HTTP_404_NOT_FOUND, 'error': 'post not found'})
@@ -100,8 +80,7 @@ class Comment(APIView):
       return Response({'status': status.HTTP_400_BAD_REQUEST, 'error': str(err)})
   
   def post(self, req, post_id=None):
-    Comments
-    comment = Comments.objects.create(user_id= 1, post_id=post_id ,content=req.data['content'])
+    comment = Comments.objects.create(user_id= 1, post_id=post_id , **req.data)
     comment = CommentSerializer(comment).data
 
     return Response({'status': status.HTTP_201_CREATED, 'comment': comment})
@@ -109,10 +88,11 @@ class Comment(APIView):
   def put(self, req, comment_id=None):
     if comment_id is not None:
       try:
+        Comments.objects.filter(id=comment_id).update(**req.data)
+
         comment = Comments.objects.get(id=comment_id)
-        comment.content = req.data['content']
-        comment.save()
         comment = CommentSerializer(comment).data
+
         return Response({'status':status.HTTP_200_OK, 'comment':comment})
       except Comments.DoesNotExist:
         return Response({'status': status.HTTP_404_NOT_FOUND, 'error': 'comment not found'})
@@ -138,8 +118,7 @@ class PostLike(APIView):
   def post(self, req, post_id):
     # user_id = req.user
     post_like = PostLikes.objects.filter(post_id=post_id, user_id=1)
-    print('len: ', len(post_like))
-    if len(post_like) < 0:
+    if len(post_like) == 0:
       PostLikes.objects.create(post_id=post_id, user_id=1)
       return Response(status=status.HTTP_200_OK)
     else:
