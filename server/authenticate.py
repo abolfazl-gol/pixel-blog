f9dc670e906b984f2ea82915ccb545a5e6f8c2d8
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-03-28 16:33:10 +0330

    chore

--- server/authenticate.py
+++ server/authenticate.py
@@ -3,5 +3,5 @@ from rest_framework.permissions import BasePermission
 
 class Authenticate(BasePermission):
   def has_permission(self, req, view):
-    print('authe user:', req)
-    return bool(req.user and req.user.is_authenticated)
+    print('user: ', req.user)
+    return req.user and req.user.is_authenticated
