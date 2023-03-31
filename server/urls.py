5000e60c115e633a9256d8684bc2a9774ff0e11f
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-03-31 17:13:04 +0330

    add `refresh` path

--- server/urls.py
+++ server/urls.py
@@ -7,6 +7,7 @@ from rest_framework_simplejwt import views as jwt_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', blog_view.Authe.as_view()),
     path('posts/',  blog_view.Post.as_view()),
     path('posts/<int:post_id>/',  blog_view.Post.as_view()),
