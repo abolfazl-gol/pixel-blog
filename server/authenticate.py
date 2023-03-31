e38ef429ff236562e19a9dd83bcfcb0fc6830794
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-03-31 17:12:40 +0330

    remove print

--- server/authenticate.py
+++ server/authenticate.py
@@ -3,5 +3,4 @@ from rest_framework.permissions import BasePermission
 
 class Authenticate(BasePermission):
   def has_permission(self, req, view):
-    print('user: ', req.user)
     return req.user and req.user.is_authenticated
