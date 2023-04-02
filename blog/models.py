791ab003e7c8165a122194390c10f6bd9ea2722c
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-04-02 15:37:36 +0330

    change tables name & model name

--- blog/models.py
+++ blog/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
-class Posts(models.Model):
+class Post(models.Model):
   title = models.CharField(max_length=255)
   content = models.TextField()
   subject = models.CharField(max_length=255)
@@ -12,35 +12,44 @@ class Posts(models.Model):
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
 
+  class Meta:
+    db_table = 'posts'
+
   def __str__(self):
     return self.title
 
 
-class Comments(models.Model):
+class Comment(models.Model):
   content = models.TextField()
   vote_post = models.IntegerField(default=0)
-  post = models.ForeignKey(Posts, db_column='post_id', on_delete=models.CASCADE)
+  post = models.ForeignKey(Post, db_column='post_id', on_delete=models.CASCADE)
   user = models.ForeignKey(User, db_column='user_id', on_delete=models.CASCADE)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
+
+  class Meta:
+    db_table = 'comments'
 
   def __str__(self):
     return self.content
 
 
-class PostLikes(models.Model):
+class PostLike(models.Model):
   user_id = models.IntegerField()
   post_id = models.IntegerField()
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
 
   class Meta:
+    db_table = 'post_likes'
     unique_together = ('user_id', 'post_id')
 
-class Images(models.Model):
-  title = models.CharField(max_length=255)
-  image = models.ImageField(upload_to='images/')
-  post = models.ForeignKey(Posts, db_column='post_id', on_delete=models.CASCADE)
+class Image(models.Model):
+  image = models.ImageField(upload_to='images')
+  post = models.ForeignKey(Post, db_column='post_id', on_delete=models.CASCADE)
+
+  class Meta:
+    db_table = 'images'
 
   def __str__(self):
     return self.title
