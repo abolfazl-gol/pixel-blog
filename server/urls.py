4083a77d759635fb6f7d86984f2a4c8ca7113d8d
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-04-03 14:08:05 +0330

    add `account` path

--- server/urls.py
+++ server/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
     path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
     path('register/', blog_view.AutheView.as_view()),
+    path('account/', blog_view.AutheView.as_view()),
     path('posts/',  blog_view.PostView.as_view()),
     path('posts/<int:post_id>/',  blog_view.PostView.as_view()),
     path('posts/<int:post_id>/like/',  blog_view.PostLikeView.as_view()),
