436f7301f50519f99c63febe2737463011f2b8db
abolfazl-gol <abolfazl.gol.6565@gmail.com>
2023-04-09 05:07:52 +0330

    add `account` app

---
+++ account/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="کاربری", related_name='profile')
+    # name = models.CharField(max_length=100, verbose_name="نام")
+    # family = models.CharField(max_length=100, null=True, verbose_name="نام خانوادگی")
+    # email = models.EmailField(max_length=100, verbose_name="ایمیل")
+
+    phone = models.CharField(max_length=11, verbose_name="شماره همراه")
+    profile_image = models.ImageField(upload_to="account/images", null=True, verbose_name="عکس پروفایل")
+    men = 1
+    women = 2
+    noidea = 3
+    gender_choices = [(men, 'مرد'), (women, 'زن'), (noidea, 'نظری ندارم')]
+    gender = models.IntegerField(choices=gender_choices, verbose_name="جنسیت", null=True)
+    born_date = models.DateField(verbose_name="تاریخ تولد", null=True)
+    credit = models.IntegerField(verbose_name="اعتبار", default=0)
+
+    def __str__(self):
+        return "نام کاربر:  {} {} ".format(self.User.first_name, self.User.last_name)
+
+    class Meta:
+        db_table = 'profiles'
+        verbose_name = "کاربر"
+        verbose_name_plural = "کاربران"
---
+++ account/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+import base.views
+from account.models import *
+from account.serializers import *
+from django.db import IntegrityError
+from app.authenticate import Authenticate
+
+
+
+class AutheView(APIView):
+    # permission_classes = (Authenticate,)
+    def get(self, request, user_id=None):
+        if request.user.is_staff:
+            if user_id is None:
+                return Response({"message": "User with this profile was not found!"},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            user = Profile.objects.get(user_id=user_id)
+            user_serializer = UserSerializer(user).data
+
+            return Response(user_serializer, status=status.HTTP_302_FOUND)
+        else:
+            # user = Profile.objects.get(id=request.user.id)           # error: permission
+
+            return Response({"message": "access denied"}, status=status.HTTP_400_BAD_REQUEST)
+
+    def post(self, request):
+        try:
+            user = User.objects.create_user(username=request.data["username"], password=request.data["password"])
+            profile = Profile.objects.create(user_id=user.id)
+            user = UserSerializer(user).data
+
+            return Response(user, status=status.HTTP_201_CREATED)
+        except IntegrityError:
+            return Response({"error": "username already exists"}, status=status.HTTP_409_CONFLICT)
+        except Exception as err:
+            return Response({'error': str(err)}, status=status.HTTP_400_BAD_REQUEST)
+
+    def put(self, req):
+
+        if req.user and req.user.is_authenticated:
+            user = User.objects.filter(id=req.user.id).update(**req.data)
+            user = User.objects.get(id=req.user.id)
+            user = UserSerializer(user).data
+
+            return Response(user, status=status.HTTP_200_OK)
+        else:
+            return Response({"message": "access denied"}, status=status.HTTP_401_UNAUTHORIZED)
+
+    def delete(self, request):
+        if request.user.is_staff:
+            try:
+                user = User.objects.get(id=request.data["id"])
+                # user.delete()
+                return Response({"message": "user {} deleted successfully".format(request.data["id"])},
+                                status=status.HTTP_200_OK)
+            except:
+                return Response({"message": "user {} not exist".format(request.data["id"])},
+                                status=status.HTTP_400_BAD_REQUEST)
+        else:
+            return Response({"message": "access denied"}, status=status.HTTP_400_BAD_REQUEST)
